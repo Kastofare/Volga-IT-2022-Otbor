1b5b01ddc584dc20fcdaddd5c8f62fe536cbd99b
Kastofare <102027859+Kastofare@users.noreply.github.com>
2022-03-26 17:00:49 +0300

    Update fairy_tail.hpp

--- fairy_tail.hpp
+++ fairy_tail.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 #include <iostream>
-#include <fstream>
+#include <fstream> 
 #include <utility>
-#include <vector>
+#include <vector> //все библиотеки
 
 enum class Character
 {
     Ivan,
     Elena,
-};
+}; //персонажи
 
 enum class Direction : char
 {
@@ -18,30 +18,30 @@ enum class Direction : char
     Down = 'D',
     Left = 'L',
     Right = 'R',
-};
+}; //направления движения
 
 class Fairyland
 {
-    using Position = std::pair<int, int>;
+    using Position = std::pair<int, int>; //где стоят Иван и Елена
 
 public:
-    explicit Fairyland();
+    explicit Fairyland(); //класс Fairyland
     ~Fairyland();
 
 public:
     int getTurnCount() const;
-    bool canGo(Character name, Direction direction) const;
-    bool go(Direction directionIvan, Direction directionElena);
+    bool canGo(Character name, Direction direction) const; //могут ли идти Иван и Елена 
+    bool go(Direction directionIvan, Direction directionElena); //идут ли Иван и Елена
 
 private:
-    static void check(bool expression, const char* message);
-    static bool move(Position& position, Direction direction);
+    static void check(bool expression, const char* message); //проверка
+    static bool move(Position& position, Direction direction); //перемещение
 
 private:
-    static const std::size_t gSize = 10;
+    static const std::size_t gSize = 10; //размер input.txt
     std::vector<std::vector<bool>> mMaze;
-    Position mIvanPos;
-    Position mElenaPos;
+    Position mIvanPos; //где стоит Иван
+    Position mElenaPos; //где стоит Елена
     std::ofstream mOutput;
-    int mTurnCount;
+    int mTurnCount; //счётчик ходов
 };
