94f569372ebf3cef54ce26672f60f9d3ef91615b
Kastofare <102027859+Kastofare@users.noreply.github.com>
2022-06-02 10:36:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 
-void generator(char v[10][10])
+void generator(char v[10][10])//в этой функции создаётся карта местности
 {
     std::ofstream file;
     file.open("input.txt", std::ios::out | std::ios::in);
@@ -56,7 +56,7 @@ void generator(char v[10][10])
 file.close();
 }
 
-int walk(int k)
+int walk(int k)//в этой функции определяется, как идёт Иван
 {
     Fairyland world;
 
@@ -65,12 +65,13 @@ int walk(int k)
         Direction direction;
 
         int k;//k - куда смотрят глаза
-        std::cout<<"do:k="<<k<<std::endl;
+        //std::cout<<"do:k="<<k<<std::endl;
         //использую правило правой руки(идти вперёд по направлению правой руки, подробнее в самой программе)
         /*k=1;//правая рука направлена влево (человек смотрит вниз и по возможности идёт вниз)
         k=2;//правая рука направлена вниз (человек смотрит вправо и по возможности идёт вправо)
         k=3;//правая рука направлена вправо (человек смотрит вверх и по возможности идёт вверх)
-        k=4;//правая рука направлена вверх (человек смотрит влево и по возможности идёт влево)*/
+        k=4;//правая рука направлена вверх (человек смотрит влево и по возможности идёт влево)
+        k=5,6;//дополнительные состояния*/
         for (int s=0;s<1;s++) {
            
         if (!(world.canGo(Character::Ivan, Direction::Left))&&(!(world.canGo(Character::Ivan, Direction::Up)))&&(!(world.canGo(Character::Ivan, Direction::Right)))) {k=1; break;}
@@ -98,8 +99,8 @@ int walk(int k)
                     case 3:direction = Direction::Up; break;
                     case 0:direction = Direction::Left; break;
             }
-        }
-        std::cout<<"posle:k="<<k<<std::endl;
+        }//здесь определяется, куда пойдёт Иван
+        //std::cout<<"posle:k="<<k<<std::endl;
         if (world.canGo(Character::Ivan, direction) && world.go(direction, Direction::Pass))
             return world.getTurnCount();
     }
@@ -112,11 +113,12 @@ int main()
     srand(static_cast<unsigned int>(time(nullptr)));
     int rezhim,turn;
     std::cout<<"How will the program work?"<<std::endl<<"Counter (Press 1)."<<std::endl<<"Generator (Press 2)."<<std::endl;
-    std::cin>>rezhim;
+    std::cin>>rezhim;//определяется, создавать ли новую карту или находить количество ходов на уже имеющейся карте 
+    //карта должна находиться в папке с проектом и называться input.txt (пример в проекте)
     std::cout<<"How many turns do you need?";
     std::cin>>turn;
     switch (rezhim)
-            {case 1:
+            {case 1://нахождение количества ходов, за которое встретятся Иван и Елена
                     if (const int turns = walk(turn)) {
                         std::cout << "Found in " << turns << " turns" << std::endl;
                     }
@@ -125,19 +127,19 @@ int main()
                 break;
                 case 2:
                     char v[10][10];
-                    generator(v);
+                    generator(v);//создание карты
                     int sch = 0;
-                    while (sch==0) {
+                    while (sch==0) {//проверка карты
                     if (const int turns = walk(turn)) {
                     std::cout << "Found in " << turns << " turns" << std::endl;
                     sch=1;
                     for (int i=0; i<10; i++) {
                        for (int j=0; j<10; j++){
                       std::cout<<v[i][j];
-                       }
+                       }//карта правильная, выводится в консоль
                     std::cout<<std::endl;}
                 }
-                    else {generator(v);}
+                    else {generator(v);}//карта неправильная, создаётся новая карта
                 }
                 break;
                 }
