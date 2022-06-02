319f6f61c1348b100a7387a671ae2b3c521c02e9
Kastofare <102027859+Kastofare@users.noreply.github.com>
2022-06-02 09:36:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void generator(char v[10][10])
     std::ofstream file;
     file.open("input.txt", std::ios::out | std::ios::in);
     char c;
-    int p=0; bool iv=false,el=false;
+    bool iv=false,el=false;
     for (int i=0;i<10;i++) {
         for (int j = 0; j < 10; j++) {
             switch (rand() % 4) {
@@ -65,13 +65,14 @@ int walk(int k)
         Direction direction;
 
         int k;//k - куда смотрят глаза
-        
+        std::cout<<"do:k="<<k<<std::endl;
         //использую правило правой руки(идти вперёд по направлению правой руки, подробнее в самой программе)
         /*k=1;//правая рука направлена влево (человек смотрит вниз и по возможности идёт вниз)
         k=2;//правая рука направлена вниз (человек смотрит вправо и по возможности идёт вправо)
         k=3;//правая рука направлена вправо (человек смотрит вверх и по возможности идёт вверх)
         k=4;//правая рука направлена вверх (человек смотрит влево и по возможности идёт влево)*/
         for (int s=0;s<1;s++) {
+           
         if (!(world.canGo(Character::Ivan, Direction::Left))&&(!(world.canGo(Character::Ivan, Direction::Up)))&&(!(world.canGo(Character::Ivan, Direction::Right)))) {k=1; break;}
         if (!(world.canGo(Character::Ivan, Direction::Left))&&(!(world.canGo(Character::Ivan, Direction::Up)))&&((world.canGo(Character::Ivan, Direction::Right)))&&(k==6)) {k=1; break;}
         if (!(world.canGo(Character::Ivan, Direction::Left))&&(!(world.canGo(Character::Ivan, Direction::Up)))&&(world.canGo(Character::Ivan, Direction::Right))) {k=2; break;}
@@ -84,21 +85,25 @@ int walk(int k)
         if (!(world.canGo(Character::Ivan, Direction::Up))&&(k==3)) {k=4; break;}
 
 
-        if (!(world.canGo(Character::Ivan, Direction::Up))&&!(world.canGo(Character::Ivan, Direction::Left))&&(k==3)) {k=5;break;}
-        }
-            
-        switch (k) {
-            case 1:direction = Direction::Down; break;
+        if (!(world.canGo(Character::Ivan, Direction::Up))&&!(world.canGo(Character::Ivan, Direction::Left))&&(k==3)) {k=5;break;}}
+            switch (k) {case 1:direction = Direction::Down; break;
             case 2:direction = Direction::Right; break;
             case 3:direction = Direction::Up; break;
             case 4:direction = Direction::Left; break;
             case 5:direction = Direction::Right; break;
             case 6:direction = Direction::Left; break;
+            default: switch (rand() % 4) {
+                    case 1:direction = Direction::Down; break;
+                    case 2:direction = Direction::Right; break;
+                    case 3:direction = Direction::Up; break;
+                    case 0:direction = Direction::Left; break;
+            }
         }
-        
+        std::cout<<"posle:k="<<k<<std::endl;
         if (world.canGo(Character::Ivan, direction) && world.go(direction, Direction::Pass))
             return world.getTurnCount();
     }
+
     return 0;
 }
 
@@ -112,16 +117,18 @@ int main()
     std::cin>>turn;
     switch (rezhim)
             {case 1:
-                    if (const int turns = walk(turn)) 
-                    {std::cout << "Found in " << turns << " turns" << std::endl;}
+                    if (const int turns = walk(turn)) {
+                        std::cout << "Found in " << turns << " turns" << std::endl;
+                    }
                     else
-                    {std::cout << "Not found" << std::endl;}
+                        std::cout << "Not found" << std::endl;
                 break;
                 case 2:
-                    char v[10][10]; generator(v);
-                int sch = 0;
-                while (sch==0) {
-                if (const int turns = walk(turn)) {
+                    char v[10][10];
+                    generator(v);
+                    int sch = 0;
+                    while (sch==0) {
+                    if (const int turns = walk(turn)) {
                     std::cout << "Found in " << turns << " turns" << std::endl;
                     sch=1;
                     for (int i=0; i<10; i++) {
@@ -130,7 +137,7 @@ int main()
                        }
                     std::cout<<std::endl;}
                 }
-                else {generator(v);}
+                    else {generator(v);}
                 }
                 break;
                 }
@@ -138,3 +145,4 @@ int main()
 
     return 0;
 }
+
